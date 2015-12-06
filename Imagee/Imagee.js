4ec31b33f2ef3af5418687c8a4abd13ec65a96eb
piratdavid <david@tizard.se>
2015-12-06 16:55:31 +0100

    Last

--- Imagee/Imagee.js
+++ Imagee/Imagee.js
@@ -4,13 +4,15 @@ Files = new Mongo.Collection("images");
 if (Meteor.isClient) {
 
 
-  // var images = [
-  //   {category: "", text: "1", img: "images/1-to go.png"},
-  //   {text: "2", img: "images/2-sadface.png"},
-  //   {text: "3", img: "images/3-to complete.png"},
-  //   {text: "4", img: "images/4-to want copycolored balls.png"},
-  //   {text: "5", img: "images/5-No in color.png"}
-  // ];
+  var images = [
+    {category: "health", text: "1", img: "images/1-to go.png"},
+    {category: "food", text: "2", img: "images/2-sadface.png"},
+    {category: "shopping", text: "3", img: "images/3-to complete.png"},
+    {category: "transportation", text: "4", img: "images/4-to want copycolored balls.png"},
+    {category: "dailylife", text: "5", img: "images/5-No in color.png"},
+    {category: "dailylife", text: "5", img: "images/3-to complete.png"},
+    {category: "dailylife", text: "5", img: "images/2-sadface.png"}
+  ];
   // var categories = [];
 
 
@@ -34,26 +36,29 @@ if (Meteor.isClient) {
     this.render('zoom');
   });
   Router.route('/categories/:_id', function() {
-    var self = this;
-    var categoryId = "";
-    Categories.find().fetch().forEach(function(obj, index) {
-      if (obj.id == self.params._id) {
-        categoryId = obj["Swedish"];
-      }
-    });
+    //var self = this;
+    // var categoryId = "";
+    // Categories.find().fetch().forEach(function(obj, index) {
+    //   if (obj.id == self.params._id) {
+    //     categoryId = obj["Swedish"];
+    //   }
+    // });
 
-    var ehj = Files.find({"Kategori": categoryId});
-    $.each(categories, function(idx, category) {
-      Blaze.renderWithData(Template.category, category, parentNode);
-    });
+    //var ehj = Files.find({"Kategori": categoryId});
+    var hej;
+
+    for (var i = 0; i < images.length; i++) {
+      Blaze.renderWithData(Template.image, images[i], $(".images"));
+    };
+
   });
 
   Template.categories.rendered = function() {
 
-    var parentNode = $(this.lastNode).find(".content .row")[1];
-    Categories.find().fetch().forEach(function(idx, category) {
-			Blaze.renderWithData(Template.category, category, parentNode);
-		});
+    // var parentNode = $(this.lastNode).find(".content .row")[1];
+    // Categories.find().fetch().forEach(function(idx, category) {
+		// 	Blaze.renderWithData(Template.category, category, parentNode);
+		// });
 
   }
 
@@ -64,16 +69,18 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function() {
 
-    if (Categories.find().count() != 0) {
-      var files = JSON.parse(Assets.getText("komHit.json"));
-      files.forEach(function (file, index, array) {
-          Files.insert(file);
-      });
-      var categories = JSON.parse(Assets.getText("categories.json"));
-      categories.forEach(function (category, index, array) {
-          Categories.insert(category);
-      });
-    }
+    // if (Files.find().count() != 0) {
+    //   var files = JSON.parse(Assets.getText("komHit.json"));
+    //   files.forEach(function (file, index, array) {
+    //       Files.insert(file);
+    //   });
+    // }
+    // if (Categories.find().count() != 0) {
+    //   var categories = JSON.parse(Assets.getText("categories.json"));
+    //   categories.forEach(function (category, index, array) {
+    //       Categories.insert(category);
+    //   });
+    // }
 
 
     Meteor.publish("allimages", function() {
