4eaf6f7bf690332f0a93a5a8534747067879d9f2
Pak <radishlau@gmail.com>
2015-12-28 06:11:05 +0800

    Selecting Category works

--- client/mainPage.js
+++ client/mainPage.js
@@ -1,12 +1,37 @@
+var stringCore = "Core";
+Session.setDefault("selectedCategory", "");
+//var selectedCategory = "Feelings";
+
 Template.mainPage.helpers({
   images: function() {
-    //console.log(test1);
-    return Images.find();
+    var categorySecondary = Session.get('selectedCategory');
+    console.log("categorySecondary is:" + categorySecondary);
+    console.log("B");
+    var objCore = Images.find({
+      category: {
+        $in: ["Core", categorySecondary]
+      }
+    }, {
+      sort: {
+        "_id": -1
+      }
+    });
+
+    console.log(objCore);
+    return objCore;
   }
 });
 
 
 Template.mainPage.events({
+  "click .category": function(event, template) {
+    var btnCategory = event.target.title;
+    console.log("btnCategory is :" + btnCategory);
+    //selectedCategory = btnCategory;
+    Session.set('selectedCategory', btnCategory);
+    console.log("selectedCategory is:" + Session.get('selectedCategory'));
+  },
+
   "click .clickImage": function(event, template) {
     //console.log(event.target.name);
     var imageId = this._id;
