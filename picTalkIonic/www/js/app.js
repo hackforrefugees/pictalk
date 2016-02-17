d7aa1034d9d7e6450a11f9d491d506a375fc6e97
Pak <radishlau@gmail.com>
2016-02-17 23:35:52 +0100

    Now users can select the languages

--- picTalkIonic/www/js/app.js
+++ picTalkIonic/www/js/app.js
@@ -6,101 +6,120 @@
 angular.module('picTalk', ['ionic'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    $ionicPlatform.ready(function() {
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if (window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+            // Don't remove this line unless you know what you are doing. It stops the viewport
+            // from snapping when text inputs are focused. Ionic handles this internally for
+            // a much nicer keyboard experience.
+            cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
 })
 
 
 
 .controller('SelectCtrl', function($scope, $ionicModal) {
-  // No need for testing data anymore
-  $scope.messages = [];
-  $scope.allPics = picInJson;
-  $scope.currentCategory = "Core";
-  $scope.pics = picInJson;
+    // No need for testing data anymore
+    $scope.messages = [];
+    $scope.allPics = picInJson;
+    $scope.currentCategory = "Core";
+    $scope.pics = picInJson;
 
-
-  // Create and load the Modal
-  $ionicModal.fromTemplateUrl('new-pic.html', function(modal) {
-    $scope.picModal = modal;
-  }, {
-    scope: $scope,
-    animation: 'slide-in-up'
-  });
-
-  // Called when the form is submitted
-  $scope.createPic = function(pic) {
-    $scope.pics.push({
-      title: pic.title
+    // Load Message Modal  ------------------------------------------- 
+    // Create and load the Message Modal
+    $ionicModal.fromTemplateUrl('message.html', function(modal) {
+        $scope.picModal = modal;
+    }, {
+        scope: $scope,
+        animation: 'slide-in-up'
     });
-    $scope.picModal.hide();
-    pic.title = "";
-  };
 
-  // Open our new pic modal
-  $scope.newPic = function() {
-    $scope.picModal.show();
-  };
+     // Open our new pic modal
+    $scope.openMessage = function() {
+        $scope.picModal.show();
+    };
 
-  // Close the new pic modal
-  $scope.closeMessage = function() {
-    $scope.picModal.hide();
-  };
+    // Close the new pic modal
+    $scope.closeMessage = function() {
+        $scope.picModal.hide();
+    };
+
+    // Set Language  ------------------------------------------- 
+    // Create and load the Language selection Modal
+    $ionicModal.fromTemplateUrl('language.html', function(modal) {
+        $scope.langModal = modal;
+    }, {
+        scope: $scope,
+        animation: 'slide-in-up'
+    });
 
 
-  $scope.deletePicInMessage = function(index) {
-    console.log("------");
-    $scope.messages.splice(index, 1);
-  };
+    // Open our new pic modal
+    $scope.openLangSel = function() {
+        $scope.langModal.show();
+    };
 
+    // Close the new pic modal
+    $scope.closeLangSel = function() {
+        $scope.langModal.hide();
+    };
 
-  $scope.picOnTap = function(tappedPic) {
-    console.log($scope.picTouched);
-    //console.log("yoyo" + tappedPic.id);
-    tappedPic.selected = true;
-    $scope.messages.push(
-      tappedPic
-    );
-    console.log($scope.messages);
-  };
-
-  $scope.picOnTouch = function() {
-    console.log("touched");
-  };
-
-  $scope.onSwipeRight = function() {
-    console.log("onSwipeRight");
-    $scope.selectCategory("Health")
-  };
-
-  $scope.selectCategory = function(selectedCategory) {
-    console.log(selectedCategory);
-    var currentPics = [];
-    $scope.currentCategory = selectedCategory;
-    for (var a = 0; a < $scope.allPics.length; a++) {
-      if ($scope.allPics[a].category == $scope.currentCategory)
-        currentPics.push($scope.allPics[a]);
+    $scope.language = {
+      native : 'swedish',
+      trans  : 'arabic'
     }
-    $scope.pics = currentPics;
-  };
+    
 
-  var onLoadPage = function() {
-    console.log("onLoadPage");
-  };
 
-  onLoadPage();
+
+
+    $scope.deletePicInMessage = function(index) {
+        console.log("------");
+        $scope.messages.splice(index, 1);
+    };
+
+
+    $scope.picOnTap = function(tappedPic) {
+        console.log($scope.picTouched);
+        //console.log("yoyo" + tappedPic.id);
+        tappedPic.selected = true;
+        $scope.messages.push(
+            tappedPic
+        );
+        console.log($scope.messages);
+    };
+
+    $scope.picOnTouch = function() {
+        console.log("touched");
+    };
+
+    $scope.onSwipeRight = function() {
+        console.log("onSwipeRight");
+        $scope.selectCategory("Health")
+    };
+
+    $scope.selectCategory = function(selectedCategory) {
+        console.log(selectedCategory);
+        var currentPics = [];
+        $scope.currentCategory = selectedCategory;
+        for (var a = 0; a < $scope.allPics.length; a++) {
+            if ($scope.allPics[a].category == $scope.currentCategory)
+                currentPics.push($scope.allPics[a]);
+        }
+        $scope.pics = currentPics;
+    };
+
+    var onLoadPage = function() {
+        console.log("onLoadPage");
+    };
+
+    onLoadPage();
 
 })
